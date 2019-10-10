d02ac0b97a9a434e0430ceb90947ac4f148dd3cd
Francis Breidenbach <fbreidenbach@gmail.com>
2019-10-09 19:40:14 -0700

    Add RGB properties w State and update preview.

--- Personal_RGBullsEye/ContentView.swift
+++ Personal_RGBullsEye/ContentView.swift
@@ -9,6 +9,15 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  let rTarget = Double.random(in: 0..<1)
+  let gTarget = Double.random(in: 0..<1)
+  let bTarget = Double.random(in: 0..<1)
+  
+  @State var rGuess: Double
+  @State var gGuess: Double
+  @State var bGuess: Double
+  
   var body: some View {
     VStack {
       VStack {
@@ -33,6 +42,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView()
+    ContentView(rGuess: 0.5, gGuess: 0.5, bGuess: 0.5)
   }
 }
