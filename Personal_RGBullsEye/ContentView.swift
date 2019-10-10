1acfca9f2bf089f5064280cb63d6bff1863f4eaf
Francis Breidenbach <fbreidenbach@gmail.com>
2019-10-09 19:36:39 -0700

    Add second VStack, Button and Slider.

--- Personal_RGBullsEye/ContentView.swift
+++ Personal_RGBullsEye/ContentView.swift
@@ -9,16 +9,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+  var body: some View {
+    VStack {
       VStack {
-        /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
-        Text("Match this Color")
+        HStack {
+          VStack {
+            /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
+            Text("Match this Color")
+          }
+          VStack {
+            /*@START_MENU_TOKEN@*/Color(red: 0.5, green: 0.5, blue: 0.5)/*@END_MENU_TOKEN@*/
+            Text("R: 127  G: 127  B: 127")
+          }
+        }
+        Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+          Text("Hit Me!")
+        }
       }
+      Slider(value: .constant(0.5))
     }
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
